e585a21ee8c7c3288fe3401b8cc277e0aef7ac48
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-08-13 13:28:27 -0300

    configure editor, linting add title

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/styles';
+import Helmet from 'react-helmet';
+import Header from './Header';
+
+const useStyles = makeStyles({
+  root: {
+    paddingTop: '40px',
+  },
+});
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      <Helmet bodyAttributes={{ style: 'background-color : #f5f5f7' }} />
+    </div>
+  );
+}
+
+export default App;
